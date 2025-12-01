ccf1e42d0a594ae8d92ff79eebc117660f8a6b88
Rodrigo-code10 <rodrigo.rolandogs@gmail.com>
2025-12-01 05:31:23 -0600

    Prueba Paginacion

--- web/js/logicaCatalogoRecetas.js
+++ web/js/logicaCatalogoRecetas.js
@@ -3,7 +3,7 @@ import API_URL from './config.js';
 
 let filtrosActuales = null;   
 let paginaActual = 1;         
-const LIMIT = 14;             
+const LIMIT = 4;             
 
 document.addEventListener('DOMContentLoaded', () => {
     const buscador = document.getElementById('buscar_recetas');
